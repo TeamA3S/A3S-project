882e8bd8efafbcd38c719c3dc0498de9d30e074e
tmdgsudl <leesh27955@gmail.com>
2026-03-20 23:34:58 +0900

    refactor : 큰일날뻔~~ㅋㅋ

--- src/main/java/com/example/a3sproject/domain/portone/PortOneClient.java
+++ src/main/java/com/example/a3sproject/domain/portone/PortOneClient.java
@@ -55,7 +55,7 @@ public class PortOneClient { // 실제 API 호출, webhook이랑 같은 로직
     ){
         return portOneRestClient
                 .post()
-                .uri("/payments/{paymentId}/cancel", paymentId)
+                .uri("/payments/{paymentId}/cancel"+storeId, paymentId)
                 .body(cancelRequest)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
