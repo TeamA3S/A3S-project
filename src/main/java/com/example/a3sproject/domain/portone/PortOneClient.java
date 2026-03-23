4daefcae7033cc15337e49c3581803b791db827f
Poten97 <ghwjd6767@gmail.com>
2026-03-23 20:00:34 +0900

    feat:빌링키 결제 URI 수정

--- src/main/java/com/example/a3sproject/domain/portone/PortOneClient.java
+++ src/main/java/com/example/a3sproject/domain/portone/PortOneClient.java
@@ -86,7 +86,7 @@ public class PortOneClient { // 실제 API 호출, webhook이랑 같은 로직
     ) {
         return portOneRestClient
                 .post()
-                .uri("/payments/{paymentId}/instant", paymentId)
+                .uri("/payments/{paymentId}/billing-key", paymentId)
                 .body(request)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, (request1, response) -> {
