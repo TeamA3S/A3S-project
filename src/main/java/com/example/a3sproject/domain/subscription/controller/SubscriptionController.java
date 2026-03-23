d55a2ba527c5a3dfe538faf0bd4412578bcaacab
hoon3948 <robot3949@gmail.com>
2026-03-23 11:28:40 +0900

    feat: 구독 기초 베이스
    Signed-off-by: hoon3948 <robot3949@gmail.com>

--- src/main/java/com/example/a3sproject/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/com/example/a3sproject/domain/subscription/controller/SubscriptionController.java
@@ -2,14 +2,14 @@ package com.example.a3sproject.domain.subscription.controller;
 
 
 import com.example.a3sproject.domain.subscription.dtos.request.CreateSubscriptionRequest;
-import com.example.a3sproject.domain.subscription.dtos.response.CreateSubscriptionReaponse;
-import com.example.a3sproject.domain.subscription.dtos.response.GetSubscriptionReaponse;
-import com.example.a3sproject.domain.subscription.dtos.response.UpdateSubscriptionReaponse;
+import com.example.a3sproject.domain.subscription.dtos.request.UpdateSubscriptionRequest;
+import com.example.a3sproject.domain.subscription.dtos.response.CreateSubscriptionResponse;
+import com.example.a3sproject.domain.subscription.dtos.response.GetSubscriptionResponse;
+import com.example.a3sproject.domain.subscription.dtos.response.UpdateSubscriptionResponse;
 import com.example.a3sproject.global.dto.ApiResponseDto;
 import com.example.a3sproject.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.tools.Trace;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,26 +17,37 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/subscription")
+@RequestMapping("/api/subscriptions")
 public class SubscriptionController {
 
     // create-subscription
     @PostMapping
-    public ResponseEntity<ApiResponseDto<CreateSubscriptionReaponse>> CreateSubscription(
+    public ResponseEntity<ApiResponseDto<CreateSubscriptionResponse>> CreateSubscription(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody CreateSubscriptionRequest request
-    ) { }
+    ) {
+    }
 
     // get-subscription
     @GetMapping("/{subscriptionId}")
-    public ResponseEntity<ApiResponseDto<GetSubscriptionReaponse>> GetSubscription( //Todo : Trace trace가 자동완성으로 들어왔는데 뭐였는지 확인할것
+    public ResponseEntity<ApiResponseDto<GetSubscriptionResponse>> GetSubscription( //Todo : Trace trace가 자동완성으로 들어왔는데 뭐였는지 확인할것
                                                                                     @AuthenticationPrincipal CustomUserDetails userDetails,
                                                                                     @PathVariable String subscriptionId
     ) {
     }
+
     // update-subscription
-    @PutMapping("/{subscriptionId}")
-    public ResponseEntity<ApiResponseDto<UpdateSubscriptionReaponse>> UpdateSubscription(
+    @PatchMapping ("/{subscriptionId}")
+    public ResponseEntity<ApiResponseDto<UpdateSubscriptionResponse>> UpdateSubscription(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable String subscriptionId,
+            @RequestBody UpdateSubscriptionRequest request
+    ) {
+
+    }
+
+    @GetMapping("/{subscriptionId}/billings")
+    public ResponseEntity<ApiResponseDto<GetSubscriptionResponse>> GetBillings(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable String subscriptionId
     ){
---
+++ src/main/java/com/example/a3sproject/domain/subscription/dtos/response/CreateSubscriptionResponse.java
@@ -0,0 +1,11 @@
+package com.example.a3sproject.domain.subscription.dtos.response;
+
+public record CreateSubscriptionResponse(
+        String subscriptionId
+) {
+}
+//name: subscriptionId
+//type: string
+//required: true
+//description: 생성된 구독 ID
+//usage: 구독 관리 페이지 리다이렉트에 사용
